f3f94dc6456866e09f0b03562c949b431bf78871
Ayush Dubey <ayush21dubey2005@gmail.com>
2026-06-26 03:30:05 +0530

    How to clean the already built projects to understand React and vite workings

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+   <h1>React first </h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  
+
+  return (
+  <h1>Ayush Reacts with vite</h1>
+  )
+}
+
+export default App
